64089ca7159425ba35ea50375041a623a68e6c38
Madalina Harii <hariimadalina@gmail.com>
2020-08-04 23:16:49 +0200

    Created models, migrations and db file

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Pet, VetOffice, Appointment, Reminder
 
-# Register your models here.
+admin.site.register(Pet)
+admin.site.register(VetOffice)
+admin.site.register(Appointment)
+admin.site.register(Reminder)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Pet(models.Model):
+
+    FEMALE = 'F'
+    MALE = 'M'
+    PET_SEX_CHOICES = [
+        (FEMALE, 'Female'),
+        (MALE, 'Male'),
+    ]
+
+    name = models.CharField(max_length=50, null=False)
+    sex = models.CharField(max_length=1, null=False, choices=PET_SEX_CHOICES, default='Female')
+    breed = models.CharField(max_length=100, null=False)
+    date_of_birth = models.DateField(null=False) # TODO add constraint date cannot be in the future
+    microchip_no = models.CharField(max_length=50, null=False)
+    passport_no = models.CharField(max_length=50)
+
+class VetOffice(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    address = models.CharField(max_length=100, null=False)
+    email = models.EmailField(null=False)
+    phone = models.CharField(max_length=50, null=False)
+
+class Appointment(models.Model):
+    pet_id = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    vet_id = models.ForeignKey(VetOffice, on_delete=models.CASCADE)
+    date = models.DateField(null=False)
+    time = models.TimeField(null=False)
+    notes = models.TextField()
+
+class Reminder(models.Model):
+    appointment_id = models.ForeignKey(Appointment, on_delete=models.CASCADE)
+    date = models.DateField(null=False)
+    time = models.TimeField(null=False)
