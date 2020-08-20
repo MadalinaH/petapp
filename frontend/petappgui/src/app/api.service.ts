54d788cdde18b979ed80c044380d0ced9801b824
Madalina Harii <hariimadalina@gmail.com>
2020-08-21 00:01:33 +0200

    Added dynamic http headers

--- frontend/petappgui/src/app/api.service.ts
+++ frontend/petappgui/src/app/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
 import { Appointment } from './models/appointment';
 import { Pet } from './models/pet';
 import { Vet } from './models/vet';
@@ -11,36 +12,38 @@ import { Vet } from './models/vet';
 export class ApiService {
 
   baseUrl = 'http://127.0.0.1:8000/';
+  token = this.cookieService.get('petapp-token');
   headers = new HttpHeaders({
-    'Content-Type': 'application/json',
-    Authorization: 'Token 3ea9fe47b035fe3e987dbf113e94e8e21c75cf07'
+    'Content-Type': 'application/json'
   });
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(
+    private cookieService: CookieService,
+    private httpClient: HttpClient) { }
 
   getPets() {
     const currentUrl = this.baseUrl + 'api/pets/';
-    return this.httpClient.get<Pet[]>(currentUrl, {headers: this.headers});
+    return this.httpClient.get<Pet[]>(currentUrl, {headers: this.getAuthHeaders()});
   }
 
   getPet(id) {
     const currentUrl = this.baseUrl + 'api/pets/' + id;
-    return this.httpClient.get<Pet>(currentUrl, {headers: this.headers});
+    return this.httpClient.get<Pet>(currentUrl, {headers: this.getAuthHeaders()});
   }
 
   getAppointments() {
     const currentUrl = this.baseUrl + 'api/appointments/';
-    return this.httpClient.get<Appointment[]>(currentUrl, {headers: this.headers});
+    return this.httpClient.get<Appointment[]>(currentUrl, {headers: this.getAuthHeaders()});
   }
 
   getAppointment(id) {
     const currentUrl = this.baseUrl + 'api/appointments/' + id;
-    return this.httpClient.get<Appointment>(currentUrl, {headers: this.headers});
+    return this.httpClient.get<Appointment>(currentUrl, {headers: this.getAuthHeaders()});
   }
 
   getVets() {
     const currentUrl = this.baseUrl + 'api/vet_offices/';
-    return this.httpClient.get<Vet[]>(currentUrl, {headers: this.headers});
+    return this.httpClient.get<Vet[]>(currentUrl, {headers: this.getAuthHeaders()});
   }
 
   loginUser(authData) {
@@ -48,6 +51,14 @@ export class ApiService {
     const currentUrl = this.baseUrl + 'auth/';
     return this.httpClient.post(currentUrl, body, {headers: this.headers});
 
+  }
+
+  getAuthHeaders() {
+    const token = this.cookieService.get('petapp-token');
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Token ${token}`
+    })
   }
 
 }
