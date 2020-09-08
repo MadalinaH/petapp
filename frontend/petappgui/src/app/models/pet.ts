bcff6a4c1fd268c873ba02182785386d2a3d0977
Madalina Harii <hariimadalina@gmail.com>
2020-09-09 00:06:49 +0200

    Added styling

--- frontend/petappgui/src/app/models/pet.ts
+++ frontend/petappgui/src/app/models/pet.ts
@@ -1,12 +1,12 @@
 export interface Pet {
-    id: number;
-    user_id: number;
-    name: string;
-    sex: string;
-    type: string;
-    breed: string;
-    date_of_birth:Date;
-    microchip_no: string;
-    passport_no: string;
-    pet_photo: string;
+  id: number;
+  user_id: number;
+  name: string;
+  sex: string;
+  type: string;
+  breed: string;
+  date_of_birth:Date;
+  microchip_no: string;
+  passport_no: string;
+  pet_photo: string;
 }
--- frontend/petappgui/src/app/models/vet.ts
+++ frontend/petappgui/src/app/models/vet.ts
@@ -1,7 +1,7 @@
 export interface Vet {
-    id: number;
-    name: string;
-    address: string;
-    email: string;
-    phone: string;
+  id: number;
+  name: string;
+  address: string;
+  email: string;
+  phone: string;
 }
